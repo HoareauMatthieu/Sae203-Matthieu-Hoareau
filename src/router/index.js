d5962ead24e0ea1a00d3daaaadf30301293b5da0
HoareauMatthieu <matthieu.hoareau@edu.univ-fcomte.fr>
2022-05-30 13:20:27 +0200

    importation focus + mention

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import ProgrammeView from '../views/ProgrammeView.vue'
 import FestivalView from '../views/FestivalView.vue'
 import ContactView from '../views/ContactView.vue'
 import ConcertView from '../views/ConcertView.vue'
+import FocusView from '../views/FocusView.vue'
+import MentionsView from '../views/MentionsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +17,8 @@ const router = createRouter({
     { path: '/festival', name: 'Festival', component: FestivalView },
     { path: '/contact', name: 'Contact', component: ContactView },
     { path: '/concert', name: 'Concert', component: ConcertView },
+    { path: '/focus', name: 'Focus', component: FocusView },
+    { path: '/mentions', name: 'Mentions', component: MentionsView },
     // ici les autre routes
   ]
 })
