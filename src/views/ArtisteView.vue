f3a985dc72c1ad5cbd87089aeb60cfe71c0966f8
HoareauMatthieu <matthieu.hoareau@edu.univ-fcomte.fr>
2022-05-30 15:25:49 +0200

    incrustation grid dans Artiste

--- src/views/ArtisteView.vue
+++ src/views/ArtisteView.vue
@@ -1,6 +1,23 @@
 <template>
-  <div class="bg-zinc-900 p-8">
+  <div class="bg-zinc-900 p-8 pt-10">
     <h1 class="pt-20 font-Anton text-3xl">Artistes</h1>
+    <div class="grid grid-cols-2 gap-4 text-center sm:grid-cols-3 lg:grid-cols-4">
+      <div class="rounded-lg bg-indigo-300 p-4 dark:bg-indigo-800 dark:text-indigo-400">1</div>
+      <div class="rounded-lg bg-indigo-300 p-4 dark:bg-indigo-800 dark:text-indigo-400">2</div>
+      <div class="rounded-lg bg-indigo-300 p-4 dark:bg-indigo-800 dark:text-indigo-400">3</div>
+      <div class="rounded-lg bg-indigo-300 p-4 dark:bg-indigo-800 dark:text-indigo-400">4</div>
+      <div class="rounded-lg bg-indigo-300 p-4 dark:bg-indigo-800 dark:text-indigo-400">5</div>
+      <div class="rounded-lg bg-indigo-300 p-4 dark:bg-indigo-800 dark:text-indigo-400">6</div>
+      <div class="rounded-lg bg-indigo-300 p-4 dark:bg-indigo-800 dark:text-indigo-400">7</div>
+      <div class="rounded-lg bg-indigo-300 p-4 dark:bg-indigo-800 dark:text-indigo-400">8</div>
+      <div class="rounded-lg bg-indigo-300 p-4 dark:bg-indigo-800 dark:text-indigo-400">9</div>
+      <div class="rounded-lg bg-indigo-300 p-4 dark:bg-indigo-800 dark:text-indigo-400">10</div>
+      <div class="rounded-lg bg-indigo-300 p-4 dark:bg-indigo-800 dark:text-indigo-400">11</div>
+      <div class="rounded-lg bg-indigo-300 p-4 dark:bg-indigo-800 dark:text-indigo-400">12</div>
+      <div class="rounded-lg bg-indigo-300 p-4 dark:bg-indigo-800 dark:text-indigo-400">13</div>
+      <div class="rounded-lg bg-indigo-300 p-4 dark:bg-indigo-800 dark:text-indigo-400">14</div>
+      <div class="rounded-lg bg-indigo-300 p-4 dark:bg-indigo-800 dark:text-indigo-400">15</div>
+    </div>
     <h1 class="pt-20 font-Anton text-3xl">DJ</h1>
   </div>
 </template>
