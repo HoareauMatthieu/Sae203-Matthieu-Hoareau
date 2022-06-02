f8dc89871c752274c6b9ba7ecd1eafc2a1e5e2f0
HoareauMatthieu <matthieu.hoareau@edu.univ-fcomte.fr>
2022-06-02 14:18:09 +0200

    underscore + redirect

--- src/components/CardApp.vue
+++ src/components/CardApp.vue
@@ -1,4 +1,9 @@
 <template>
+
+<div v-for="artiste in listeArtiste" :key="artiste.id" :cat="artiste.cat" ><img class="rounded-xl w-full" :src="artiste.photo" :alt="artiste.nom"/></div>
+
+
+
   <div class="rounded-md border-2 dark:border-white border-black">
     <div class="p-3">
       <h2 class="pb-4 font-Anton text-2xl uppercase text-red-400 sm:text-3xl lg:text-4xl">{{ date }}</h2>
@@ -21,6 +26,27 @@
 
 <script>
 import Bouton from "../components/icons/Bouton.vue";
+
+import {
+  getFirestore, // Obtenir le Firestore
+  collection, // Utiliser une collection de documents
+  doc, // Obtenir un document par son id
+  getDocs, // Obtenir la liste des documents d'une collection
+  addDoc, // Ajouter un document à une collection
+  updateDoc, // Mettre à jour un document dans une collection
+  deleteDoc, // Supprimer un document d'une collection
+  onSnapshot, // Demander une liste de documents d'une collection, en les synchronisant
+  query, // Permet d'effectuer des requêtes sur Firestore
+  orderBy, // Permet de demander le tri d'une requête query
+} from "https://www.gstatic.com/firebasejs/9.7.0/firebase-firestore.js";
+
+// Cloud Storage : import des fonctions
+import {
+  getStorage, // Obtenir le Cloud Storage
+  ref, // Pour créer une référence à un fichier à uploader
+  getDownloadURL, // Permet de récupérer l'adress complète d'un fichier du Storage
+  uploadString, // Permet d'uploader sur le Cloud Storage une image en Base64
+} from "https://www.gstatic.com/firebasejs/9.7.0/firebase-storage.js";
 export default {
   components: { Bouton },
   props: {
@@ -29,5 +55,49 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      listeArtiste: [], // Liste des artistes
+    };
+  },
+  
+mounted() {
+    // Montage de la vue
+    // Appel de la liste des artistes
+    this.getArtiste();
+  },
+  methods: {
+    async getArtiste() {
+      // Obtenir Firestore
+      const firestore = getFirestore();
+      // Base de données (collection)  document artistes
+      const dbArtiste = collection(firestore, "artiste");
+      // Liste des artistes triés sur leur nom
+      const q = query(dbArtiste, orderBy("cat", "asc"));
+      await onSnapshot(q, (snapshot) => {
+        this.listeArtiste = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+        // Récupération des images de chaque artiste
+        // parcours de la liste
+        this.listeArtiste.forEach(function (personne) {
+          // Obtenir le Cloud Storage
+          const storage = getStorage();
+          // Récupération de l'image par son nom de fichier
+          const spaceRef = ref(storage, "artiste/" + personne.photo);
+          // Récupération de l'url complète de l'image
+          getDownloadURL(spaceRef)
+            .then((url) => {
+              // On remplace le nom du fichier
+              // Par l'url complète de la photo
+              personne.photo = url;
+            })
+            .catch((error) => {
+              console.log("erreur downloadUrl", error);
+            });
+        });
+      });
+    },
+  },
 };
+
+
 </script>
