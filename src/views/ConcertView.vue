d46e8b23b806e01e7edf525a8c59647828dbabc3
HoareauMatthieu <matthieu.hoareau@edu.univ-fcomte.fr>
2022-06-03 09:16:20 +0200

    import fonction supp

--- src/views/ConcertView.vue
+++ src/views/ConcertView.vue
@@ -11,6 +11,22 @@
       </div>
     </div>
     <div class="my-11 grid grid-cols-8 gap-2 text-center">
+      <img class="col-span-5 row-span-2 rounded-lg" src="/image/theweeknd.jpg" alt="The Weeknd">
+        <img class="col-span-3 row-span-2 rounded-lg" src="/image/laylow2.jpg" alt="laylow">
+    <img class="col-span-2 rounded-lg" src="/image/biscuit.jpg" alt="Petit Biscuit">
+    <img class="col-span-2 rounded-lg" src="/image/dj.jpg" alt="Chainsmok">
+
+    <img  class="col-span-2 rounded-lg"  src="/image/freeze.jpg" alt="Freeze Corleone">
+    <img class="col-span-2 rounded-lg" src="/image/leto.jpg" alt="leto">
+    <img class="col-span-3 rounded-lg" src="/image/luj.jpg" alt="lujipeka">
+    <img class="col-span-2 rounded-lg" src="/image/naps.jpg" alt="naps">
+    <img class="col-span-3 rounded-lg" src="/image/postmalone.jpg" alt="Post Malone">
+    <img class="col-span-3 row-span-2 rounded-lg" src="/image/shay.jpg" alt="Shay">
+    <img class="col-span-3 row-span-2 rounded-lg" src="/image/snake.jpg" alt="Dj Snake">
+    
+    <img class="col-span-2 row-span-2 rounded-lg" src="/image/zola.jpg" alt="Zola">
+
+
       <div class="col-span-5 row-span-2 rounded-lg bg-indigo-300 p-4 dark:bg-indigo-800 dark:text-indigo-400">1</div>
       <div class="col-span-3 row-span-2 rounded-lg bg-indigo-300 p-4 dark:bg-indigo-800 dark:text-indigo-400">2</div>
       <div class="col-span-2 rounded-lg bg-indigo-300 p-4 dark:bg-indigo-800 dark:text-indigo-400">3</div>
@@ -24,6 +40,9 @@
       <div class="col-span-3 row-span-2 rounded-lg bg-indigo-300 p-4 dark:bg-indigo-800 dark:text-indigo-400">11</div>
       <div class="col-span-2 row-span-2 rounded-lg bg-indigo-300 p-4 dark:bg-indigo-800 dark:text-indigo-400">12</div>
     </div>
+
+  
+    
     <h2 class="pb-5 text-center font-Anton text-2xl sm:text-3xl lg:text-6xl">À L’AFFICHE DU CONCERT</h2>
     <p class="px-12 text-center text-sm lg:px-64 lg:text-base">
       The Weeknd - DJ Snake - Leto - Naps - Jul - Lujipeka - La Fève - Freeze Corlorleone - Zola - Post Malone - Angèle - Shay + Quelques
