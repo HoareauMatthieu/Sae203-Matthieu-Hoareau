8bbed1bae2202dbfb0dfa6db54fb9c164faea8d7
HoareauMatthieu <matthieu.hoareau@edu.univ-fcomte.fr>
2022-06-03 15:46:40 +0200

    Ajout map

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -12,10 +12,10 @@
     <p class="py-8 lg:text-center lg:text-xl">Ou ici !</p>
 
     <div class="w-full">
-      <form class="mb-4 rounded pt-6 pb-8  lg:px-80">
+      <form class="mb-4 rounded pt-6 pb-8 lg:px-80">
         <div class="mb-4">
           <input
-            class="w-full appearance-none rounded border bg-transparent py-2 px-3 leading-tight "
+            class="w-full appearance-none rounded border bg-transparent py-2 px-3 leading-tight"
             id="prenom"
             type="text"
             placeholder="Prénom"
@@ -23,7 +23,7 @@
         </div>
         <div class="mb-4">
           <input
-            class="w-full appearance-none rounded border bg-transparent py-2 px-3 leading-tight "
+            class="w-full appearance-none rounded border bg-transparent py-2 px-3 leading-tight"
             id="nom"
             type="text"
             placeholder="Nom"
@@ -39,7 +39,7 @@
         </div>
         <div class="mb-4">
           <input
-            class="w-full appearance-none rounded border bg-transparent py-2 px-3 leading-tight "
+            class="w-full appearance-none rounded border bg-transparent py-2 px-3 leading-tight"
             id="tel"
             type="text"
             placeholder="Tel :"
@@ -47,7 +47,7 @@
         </div>
         <div class="mb-4">
           <input
-            class="w-full appearance-none rounded border bg-transparent py-2 px-3 leading-tight "
+            class="w-full appearance-none rounded border bg-transparent py-2 px-3 leading-tight"
             id="objet"
             type="text"
             placeholder="Objet"
@@ -55,18 +55,26 @@
         </div>
         <div class="mb-4">
           <textarea
-            class="w-full appearance-none rounded border bg-transparent py-2 px-3  leading-tight h-48 "
+            class="h-48 w-full appearance-none rounded border bg-transparent py-2 px-3 leading-tight"
             id="texte"
             type="text"
-            
             placeholder="Texte"
           />
         </div>
-        <boutonred  >Envoyer</boutonred>
+        <boutonred>Envoyer</boutonred>
       </form>
     </div>
     <h1 class="font-Anton text-3xl">Ou nous trouver ?</h1>
-    <img src="/image/carte.png" alt="carte" class="ml-auto mr-auto w-2/3 lg:w-1/3" />
+    <iframe
+      class="ml-auto mr-auto w-full"
+      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7964.763601589316!2d2.3165431737560462!3d48.812577631436724!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e670ff0c81975d%3A0x650af317b609bc1a!2s12%20Rue%20de%20Saisset%2C%2092120%20Montrouge!5e0!3m2!1sfr!2sfr!4v1654263840785!5m2!1sfr!2sfr"
+      width="600"
+      height="450"
+      style="border: 0"
+      allowfullscreen=""
+      loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"
+    ></iframe>
     <p class="py-8 text-center text-sm">12 Rue de Saisset - Montrouge 92120</p>
   </div>
 </template>
