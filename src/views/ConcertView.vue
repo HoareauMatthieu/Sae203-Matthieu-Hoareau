26ee588f4a34586c320007bafcaa23d6cd85566a
HoareauMatthieu <matthieu.hoareau@edu.univ-fcomte.fr>
2022-06-03 15:25:09 +0200

    modification concert (responsive)

--- src/views/ConcertView.vue
+++ src/views/ConcertView.vue
@@ -11,29 +11,21 @@
       </div>
     </div>
 
-    <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(280px,1fr))] gap-3">
-      <div class="rounded-lg border-2 border-white p-3" v-for="[date, listeDate] in artistesParDate" :key="date">
-        <div class="grid grid-cols-3 gap-2">
-          <img class="w-full rounded-lg" v-for="artiste in listeDate" :key="artiste.id" :src="artiste.photo" :alt="artiste.nom" />
-        </div>
-      </div>
-    </div>
+    <div class="my-11 grid grid-cols-4 gap-3 p-3 text-center lg:grid-cols-5">
+      <img class="w-full rounded-lg" src="/image/theweeknd.jpg" alt="The Weeknd" />
+      <img class="w-full rounded-lg" src="/image/laylow2.jpg" alt="laylow" />
+      <img class="w-full rounded-lg" src="/image/biscuit.jpg" alt="Petit Biscuit" />
+      <img class="w-full rounded-lg" src="/image/dj.jpg" alt="Chainsmok" />
 
-    <div class="my-11 grid grid-cols-4 gap-2 text-center">
-      <img class="rounded-lg" src="/image/theweeknd.jpg" alt="The Weeknd" />
-      <img class="rounded-lg" src="/image/laylow2.jpg" alt="laylow" />
-      <img class="rounded-lg" src="/image/biscuit.jpg" alt="Petit Biscuit" />
-      <img class="rounded-lg" src="/image/dj.jpg" alt="Chainsmok" />
+      <img class="w-full rounded-lg" src="/image/freeze.jpg" alt="Freeze Corleone" />
+      <img class="w-full rounded-lg" src="/image/leto.jpg" alt="leto" />
+      <img class="w-full rounded-lg" src="/image/luj.jpg" alt="lujipeka" />
+      <img class="w-full rounded-lg" src="/image/naps.jpg" alt="naps" />
+      <img class="w-full rounded-lg" src="/image/postmalone.jpg" alt="Post Malone" />
+      <img class="w-full rounded-lg" src="/image/shay.jpg" alt="Shay" />
+      <img class="w-full rounded-lg" src="/image/snake.jpg" alt="Dj Snake" />
 
-      <img class="rounded-lg" src="/image/freeze.jpg" alt="Freeze Corleone" />
-      <img class="rounded-lg" src="/image/leto.jpg" alt="leto" />
-      <img class="rounded-lg" src="/image/luj.jpg" alt="lujipeka" />
-      <img class="rounded-lg" src="/image/naps.jpg" alt="naps" />
-      <img class="rounded-lg" src="/image/postmalone.jpg" alt="Post Malone" />
-      <img class="rounded-lg" src="/image/shay.jpg" alt="Shay" />
-      <img class="rounded-lg" src="/image/snake.jpg" alt="Dj Snake" />
-
-      <img class="rounded-lg" src="/image/zola.jpg" alt="Zola" />
+      <img class="w-full rounded-lg" src="/image/zola.jpg" alt="Zola" />
     </div>
 
     <h2 class="pb-5 text-center font-Anton text-2xl sm:text-3xl lg:text-6xl">À L’AFFICHE DU CONCERT</h2>
